8389ae208db8b3f4eabc1a2652e70724dfcb56a6
Jared Ruiz <jared707@gmail.com>
2025-03-03 17:42:59 -0600

    Added Player componenet and passed the necessary info as props to dynamically present each player.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player/Player";
 
 function App() {
-  
-
   return (
-    <p>Coming Soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
+        </ol>
+        GAMEBOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
