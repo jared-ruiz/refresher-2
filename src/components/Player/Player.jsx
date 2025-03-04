d1dec9d5c76acafb33bfebb5b0259b07552f14b6
Jared Ruiz <jared707@gmail.com>
2025-03-03 18:37:14 -0600

    Added state and conditional logic to the player component to show user that the name edit button is working. I will start working toward the input field functionality shortly.

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  //function to handle edit click
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+//storing jsx logic in variable and dynamically loading it in return statement
+  let playerName = <span className="player-name">{name}</span>
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name}></input>
+
+}
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : "Edit"}</button>
     </li>
   );
 }
